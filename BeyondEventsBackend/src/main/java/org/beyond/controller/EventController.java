318917102e58a308fe3cb8aec5a15078668f685a
CCp0 <chriscurran01@gmail.com>
2025-08-31 11:49:10 +0100

    CORS addition and swagger fix

--- BeyondEventsBackend/src/main/java/org/beyond/controller/EventController.java
+++ BeyondEventsBackend/src/main/java/org/beyond/controller/EventController.java
@@ -3,6 +3,7 @@ package org.beyond.controller;
 import org.beyond.model.Event;
 import org.beyond.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EventController {
 
     @Autowired
